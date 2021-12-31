5e33de3883ac3f0a0019d524f3a481f1f061d733
ErikRobles <erikjames69@hotmail.com>
2021-12-30 19:33:00 -0600

    First Commit

---
+++ src/components/ComponentWithUseReducer.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DEPOSIT':
+      return state + action.payload;
+    case 'WITHDRAW':
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
+const ComponentWithUseReducer = () => {
+  const deposit = (amount) => {
+    dispatch({
+      type: 'DEPOSIT',
+      payload: amount,
+    });
+  };
+  const withdraw = (amount) => {
+    dispatch({
+      type: 'WITHDRAW',
+      payload: amount,
+    });
+  };
+  const [amount, dispatch] = useReducer(reducer, 500);
+  return (
+    <div>
+      <h1>{amount}</h1>
+      <button onClick={() => deposit(500)}>Deposit</button>
+      <button onClick={() => withdraw(100)}>Withdraw</button>
+    </div>
+  );
+};
+
+export default ComponentWithUseReducer;
